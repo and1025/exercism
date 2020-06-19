5e30234ab6180a77b40a29a231cd45b160d155bd
and1025 <fornarisandres240@gmail.com>
2020-06-18 22:22:50 -0500

    cue ejercicio resuelto

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,17 +1,13 @@
 
 import unittest
 
-def count_words(sentence):
-    counts = dict()
-    words = sentence.split()
-
-    for word in words:
-        if word in counts:
-            counts[word] += 1
-        else:
+def count_words(phrase):  
+    counts = {}
+    for word in ''.join(
+            map(lambda c: c.lower() if c.isalnum() else ' ', phrase)).split():
+        try:
+            counts[word] = counts[word] + 1
+        except KeyError:
             counts[word] = 1
 
     return counts
-
-if __name__ == "__main__":
-    main()
